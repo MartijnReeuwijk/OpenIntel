5898e152ed637c7157b310a14dfd891fb630fc57
Martijn <m.reeuwijk@Live.nl>
2019-01-08 12:19:52 +0100

    test

--- app.js
+++ app.js
@@ -40,14 +40,9 @@ app.use(express.static("static"));
 app.get("/", (req, res) => res.sendFile("/index.html", {root: __dirname }));
 
 app.get("/data", (req, res) => {
-<<<<<<< HEAD
-  readAllData().then(data => res.send(data))
-
-=======
   readAllData().then(data => {
     res.send(cleaner(data))
   });
->>>>>>> e3598500c4a8a7eb85a4707cde46687a783a957e
 })
 
 app.listen(3000, () => {
--- static/js/jesse.js
+++ static/js/jesse.js
@@ -1,97 +1,6 @@
 async function jesse() {
 let newData;
 
-<<<<<<< HEAD
-
-await d3.json("http://localhost:3000/data").then(rawData => {
-
-  rawData.forEach(chunk => {
-    let country = Object.keys(chunk);
-
-    chunk[country].forEach(d => {
-      d.date = new Date(parseInt(d.date));
-      d.all = {};
-
-      d.values.forEach(dv => {
-        let tld = dv.name.split(".").reverse()[1];
-
-        if (!d.all[tld]) {
-
-          d.all[tld] = {
-            cnt: 1,
-            total: dv.value
-          }
-
-        } else {
-
-          d.all[tld].cnt++;
-          d.all[tld].total += dv.value;
-
-        }
-      })
-
-      let temp = 0;
-
-      for (let t in d.all) {
-        temp += d.all[t].total;
-      }
-
-      for (let t in d.all) {
-        d.all[t].percentage = Number((d.all[t].total / temp * 100).toFixed(2));
-      }
-    })
-
-    chunk[country] = chunk[country].filter(c => {
-      if (c.date.getDate() == 1) {
-        return true
-      }
-    })
-
-    // newData.push(chunk);
-  })
-
-  newData = rawData;
-})
-//       d.date = new Date(parseInt(d.date));
-//       d.all = {}
-//
-//       d.values.forEach(dv => {
-//         let tld = dv.name.split(".").reverse()[1]
-//
-//         if (!d.all[tld]) {
-//
-//           d.all[tld] = {
-//             cnt: 1,
-//             total: dv.value
-//           }
-//         } else {
-//
-//           d.all[tld].cnt++;
-//           d.all[tld].total += dv.value;
-//         }
-//       })
-//
-//       let temp = 0;
-//
-//       for (let t in d.all) {
-//         temp += d.all[t].total;
-//       }
-//
-//       for (let t in d.all) {
-//         d.all[t].percentage = Number((d.all[t].total / temp * 100).toFixed(2));
-//       }
-//
-//       if (d.date.getDate() == 1) {
-//         newData.push(d)
-//       }
-//
-//     });
-//   })
-// })
-
-// New Data can be used from here
-console.log(newData)
-=======
 await d3.json("http://localhost:3000/data").then(data => {
   newData = data;
 });
@@ -124,5 +33,4 @@ let testingData = pie(newData[0]["at"][0].all);
       .append("path")
       .attr("d", arc)
       .attr("fill", (d, i) => color(i))
->>>>>>> e3598500c4a8a7eb85a4707cde46687a783a957e
 }
