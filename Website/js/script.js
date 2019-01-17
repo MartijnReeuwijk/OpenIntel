0e07afa6511af1f2d84b34151b3509c605f93000
Martijn <m.reeuwijk@Live.nl>
2019-01-17 15:20:09 +0100

    Mergin site inproggress

---
+++ Website/js/script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: "start"
+        });
+    });
+});
+
+AOS.init({
+  duration: 1000,
+})
