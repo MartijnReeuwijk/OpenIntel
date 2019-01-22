19420eb90aaaf000c2bce32947cbc634b26dbb23
Martijn <m.reeuwijk@Live.nl>
2019-01-22 11:06:27 +0100

    test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const cors = require("cors");
 const cleaner = require("./dataCleaner.js");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 function readAllData() {
   return new Promise((resolveAll, rejectAll) => {
